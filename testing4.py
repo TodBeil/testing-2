d500b2349c77da963e4bf8e1ac968220d24e021c
muffinhavlop <101418643+MuffinHavlop@users.noreply.github.com>
2023-11-06 18:04:27 +0700

    create testing4.py

---
+++ testing4.py
@@ -0,0 +1,4 @@
+age = int(input("ban sinh nam bao nhieu?: "))
+year = int(input("nam nay la nam bao nhieu?: "))
+tuoi = int(input("nam nay ban bao nhieu tuoi?: "))
+print(f"vao nam {year}, ban se {tuoi} tuoi")
